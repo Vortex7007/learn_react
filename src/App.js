65bd1d4d13b428988fcba76e37adb6c1e30a1128
anshu_mandal <anshukrmandal7007@gmail.com>
2024-07-07 21:46:11 +0530

    alert done + js to correct word count

--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
   const [mode,setMode]=useState('light');
+  const [alert, setAlert]=useState(null);
+  const showAlert = (type, message)=>{
+    setAlert({
+      typ:type,
+      msg:message
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },1000)
+  }
   const toggleMode =()=>{
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor="#061d3f";
+      showAlert("success","Dark mode has been enabled");
     }
     else{
       setMode('light')
       document.body.style.backgroundColor="white";
+      showAlert("success","Light mode has been enabled");
     }
   }
   return (
     <>
 <Navbar title="TextUtils" aboutText="acordion" mode={mode} toggleMode={toggleMode}/>
-  <TextForm heading="Enter text to analyze" mode={mode}/>
+<Alert alert={alert}/>
+  <TextForm showAlert={showAlert} heading="Enter text to analyze" mode={mode}/>
 <div className='container'>
   {/* <About/>     */}
 </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,42 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
+    const countwords=(string)=>{
+        if(string===""){
+            return 0;
+        }
+        else{
+            let str = string.replace(/\s+/g, ' ').trim();
+            str=str.split(" ").length;
+            return str;
+        }
+    }
+    const countchar=(string)=>{
+        string = string.replace(/\s+/g, ' ').trim();
+        string = string.length;
+        return string;
+    }
     const capitalizeFirstLetter = () => {
         let newText = text.replace(/\b\w/g, (char) => char.toUpperCase());
         setText(newText);
+        props.showAlert("success","Text has been set to camelcase");
       };
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("success","Text has been set to uppercase");
     }
     const handleLoClick = ()=>{
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("success","Text has been set to Lowercase");
     }
     const handleOnChange = (event)=>{
         setText(event.target.value)
     }
     const [text ,setText] =useState('');
     return (
-    <><div style={{color: props.mode==='dark'?'white':'#061d3f'}}>
+        <><div style={{color: props.mode==='dark'?'white':'#061d3f'}}>
 
    
         <div className="mb-3 container">
@@ -32,7 +50,7 @@ export default function TextForm(props) {
         </div>
          <div className="container">
             <h2>Your text summary </h2>
-            <p>{text.split(" ").length} words and {text.length} characters </p>
+            <p>{countwords(text)} words and {countchar(text)} characters </p>
             <p>Estimated time to read it = {0.005 * text.split(" ").length}</p>
             <h2>Preview</h2>
             <p>{text}</p>
